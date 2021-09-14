10acdbacda57d4d855b332e36ac503946acc418f
megoRU <savin.yura2014@gmail.com>
2021-09-14 14:03:10 +0300

    1. Fix url 2

--- src/main/java/main/service/RestoreService.java
+++ src/main/java/main/service/RestoreService.java
@@ -45,9 +45,9 @@ public class RestoreService {
                     email.get().getEmail(),
                     "DevPub: Запрос на восстановление пароля",
                     "\nВы отправили запрос на восстановление пароля на сайте: " +
-                            "https://" + InetAddress.getLoopbackAddress().getHostAddress() +
+                            "https://app-skillbox-diplom.herokuapp.com" +
                             "\nДля того чтобы задать новый пароль, перейдите по ссылке: \n" +
-                            "https://" + InetAddress.getLoopbackAddress().getHostAddress() +
+                            "https://app-skillbox-diplom.herokuapp.com" +
                             "/login/change-password/" + uuid.toString().replaceAll("-", "")
 
                             + "\n\nСсылка действительна до " + simpleDateFormat.format(calendar.getTime())
